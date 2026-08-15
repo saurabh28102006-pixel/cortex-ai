9884263dd3e3d0324715552ae6a630c001dab816
Saurabh kumar <saurabh2732006@gmail.com>
2026-08-15 21:54:38 +0530

    fix: support both VITE_SERVER_URL and VITE_API_URL

--- frontend/utils/axios.js
+++ frontend/utils/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const api=axios.create({
-    baseURL:import.meta.env.VITE_SERVER_URL,
-    withCredentials:true
-})
+const api = axios.create({
+    baseURL: import.meta.env.VITE_SERVER_URL || import.meta.env.VITE_API_URL || "http://localhost:8000",
+    withCredentials: true
+});
 
-export default api
+export default api;
 
